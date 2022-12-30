24bdbe104f0bef77781744fd55db65094edc6562
Ayrton Silva <ayrtonricardo2000@hotmail.com>
2022-12-30 15:00:37 -0300

    Finalizando projeto-02

---
+++ projeto-02/02-ignite-timer/src/App.tsx
@@ -0,0 +1,22 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { CyclesContextProvider } from './contexts/CyclesContext'
+// import { ContextExample } from './ContextExample'
+
+export function App() {
+  // return <ContextExample />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
